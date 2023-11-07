bbb9f6466d30fba2353cb3f2d3f122d6b291494f
DESKTOP-4V6A54V\RIO <rfazri7@gmail.com>
2023-11-08 00:27:13 +0700

    Fecthing dan Mapping Data Anime

---
+++ src/app/search/page.js
@@ -0,0 +1,13 @@
+// export default function Page(){
+//     return <div>SEARCH PAGE</div>
+// }
+const Page = () => {
+    return(
+        <>
+            <h1>Search Page arrow Function</h1>
+            <p>Ini adalah Search Page</p>
+        </>
+    )
+}
+
+export default Page
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return(
+        <header className="bg-indigo-500">
+            <div className="flex md:flex-row flex-col justify-between p-4 gap-2">
+                <Link href="/" className="font-bold text-white text-2xl">RIOANIMELIST</Link>
+                <input placeholder="  Cari Anime.." className="rounded-lg"/>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
