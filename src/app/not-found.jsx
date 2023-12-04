a09c5b7f15fd90c1d8fede89293685ae1df591b8
Rio Fazri Septian <rfazri7@gmail.com>
2023-12-04 16:23:52 +0700

    Membuat Menu Collection menggunakan ORM Prisma

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,14 +2,16 @@
 
 import { FileSearch } from "@phosphor-icons/react/dist/ssr"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const Page = () =>{
+    const router = useRouter()
     return(
         <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
             <div className="flex justify-center items-center gap-4 flex-col">
                 <FileSearch size={32} className="text-color-accent"/>
-                <h3 className="text-color-accent text-4xl font-bold">404 - ANIME NOT FOUND</h3>
-                <Link href="/" className="text-color-primary hover:text-color-accent transition-all underline">Kembali ke Halaman Utama</Link>
+                <h3 className="text-color-accent text-4xl font-bold">404 - NOT FOUND</h3>
+                <button onClick={()=>router.back()} className="text-color-primary hover:text-color-accent transition-all underline">Kembali</button>
             </div>
         </div>
     )
--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,44 +1,30 @@
 import Header from "@/components/Dashboard/Header"
+import { authUserSession } from "@/libs/auth-libs"
 import Image from "next/image"
 import Link from "next/link"
+import prisma from "@/libs/prisma"
 
-const Page = () =>{
+const Page = async() =>{
+    const user = await authUserSession()
+    const collection = await prisma.collection.findMany({where: {user_email:user.email}})
+
     return(
         <section className="mt-4 w-full px-4">
             <Header title={"My Collection"}/>
             <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">
-                            Judul Anime
-                        </h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">
-                            Judul Anime
-                        </h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">
-                            Judul Anime
-                        </h5>
-                    </div>
-                </Link>
-                <Link href="/" className="relative border-2 border-color-accent">
-                    <Image src="" alt="" width={350} height={350} className="w-full"/>
-                    <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                        <h5 className="text-xl text-center">
-                            Judul Anime
-                        </h5>
-                    </div>
-                </Link>
+                {collection.map((collect, index) => {
+                    return(
+                        <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative border-2 border-color-accent">
+                            <Image src="" alt="" width={350} height={350} className="w-full"/>
+                                <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
+                                    <h5 className="text-xl text-center">
+                                        {/* {anime.data.title} */}
+                                    </h5>
+                                </div>
+                            </Link>
+                    )
+                })}
+                
             </div>
         </section>
     )
