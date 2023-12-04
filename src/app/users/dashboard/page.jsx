5a87bbc1014a360f38e30b37bfc4e533814cf5b0
Rio Fazri Septian <rfazri7@gmail.com>
2023-12-04 10:38:51 +0700

    Menambahkan validasi auth user dan menu my collection

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,17 +1,21 @@
 import { authUserSession } from "@/libs/auth-libs"
-import { redirect } from "next/dist/server/api-utils"
 import Image from "next/image"
+import Link from "next/link"
 
-const page = async() => {
+const Page = async() => {
     const user = await authUserSession()
+
     return(
-        <div className="text-color-primary">
-            <h3>DASHBOARD</h3>
-            <h5>Welcome, {user.name}</h5>
-            <Image src={user.image} alt="..." width={250} height={250} className="rounded-xl"/>
+        <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Welcome, {user?.name}</h5>
+            <Image src={user?.image} alt="..." width={250} height={250} className="rounded-xl"/>
+            <div className="flex flex-wrap gap-4 py-8">
+                <Link href="/users/dashboard/collection" className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl">My Collection</Link>
+                <Link href="/users/dashboard/comment" className="bg-color-accent text-color-dark font-bold px-4 py-3 text-xl">My Comment</Link>
+            </div>
         </div>
         
     )
 }
 
-export default page
+export default Page
